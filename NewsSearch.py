2c75215e388fe591a243fe90ec8c5d92a97854d8
Sheng Hsuan Lin <shlin@Shengs-Air.Home>
2014-12-28 00:38:32 +0800

    googleAPI_added

--- NewsSearch.py
+++ NewsSearch.py
@@ -1,8 +1,12 @@
 import urllib2
 import simplejson
 
-url = ('https://ajax.googleapis.com/ajax/services/search/news?' + 'v=1.0&userip=INSERT-USER-IP&topic=h')
-#url = ('https://ajax.googleapis.com/ajax/services/search/news?' + 'v=1.0&q=barack%20obama&userip=INSERT-USER-IP')
+###Search
+###url = ('https://ajax.googleapis.com/ajax/services/search/news?' + 'v=1.0&q=barack%20obama&userip=INSERT-USER-IP')
+
+
+#
+url = ('https://ajax.googleapis.com/ajax/services/search/news?' + 'v=1.0&userip=INSERT-USER-IP&topic=h&rsz=8&ned=tw')
 
 
 request = urllib2.Request(url, None)
@@ -10,5 +14,28 @@ response = urllib2.urlopen(request)
 
 # Process the JSON string.
 results = simplejson.load(response)
-print results
+for a in results:
+    print a
+data = results['responseData']
+details = results['responseDetails']
+status = results['responseStatus']
+print " - - - - - - - "
+dataCursor = data['cursor']
+dataResults = data['results']
+for a in dataResults:
+    print a['title']
+    print "contest:", a['content']
+    
+    try:
+        related = a['relatedStories']
+        print "relatedUrl:"
+        for ele in related:
+            print ele['titleNoFormatting']
+    except:
+        pass
+
+    print ""
+ 
+
+                
 # now have some fun with the results...
